1518571e3b7f4f5e77ffa8bdba65a0251cf9fb59
Dhruv1249 <dhr1249.lm@gmail.com>
2026-01-30 11:12:23 +0530

    LSP and autocompletions now working

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,71 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        -- 1. Setup Mason (The installer)
+        require("mason").setup()
+
+        -- 2. Setup standard LSP capabilities
+        local lspconfig = require("lspconfig")
+        local capabilities = require('lspconfig').util.default_config.capabilities
+
+        -- 3. Setup Mason-LSPConfig (The bridge)
+        require("mason-lspconfig").setup({
+            -- A list of servers to automatically install
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "ts_ls",
+                "html",
+                "cssls",
+                "bashls",
+                "jsonls",
+            },
+            
+            -- OLD: setup_handlers(...) -> REMOVED
+            -- NEW: handlers = { ... } inside setup()
+            handlers = {
+                -- The default handler: setup every server with default settings
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+
+                -- Specific overrides for Lua
+                ["lua_ls"] = function()
+                    lspconfig.lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" },
+                                },
+                            },
+                        },
+                    })
+                end,
+            }
+        })
+
+        -- 4. Keybindings (Global LSP mappings)
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end,
+        })
+    end
+}
