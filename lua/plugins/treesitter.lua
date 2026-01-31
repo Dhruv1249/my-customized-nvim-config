03b4b42b4e9fb0984bf613ed2da420df1df74fcd
Dhruv1249 <dhr1249.lm@gmail.com>
2026-01-31 15:40:47 +0530

    added toggle for autosave and treesitter now works properly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,75 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-	-- 1. FORCE the installation directory to a known standard path
-    --    This makes sure the parsers are where Neovim expects them.
-    local parser_install_dir = vim.fn.stdpath("data") .. "/site"
-    
-    -- 2. MANUALLY add that directory to Neovim's "runtime path"
-    --    This fixes the "not in runtimepath" error from your screenshot.
-    vim.opt.rtp:prepend(parser_install_dir)
-      require("nvim-treesitter.config").setup({
-	parser_install_dir = parser_install_dir,
-        -- Automatically install parsers for these languages
-        ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "c","cpp","python","tsx","css",
-	"json","yaml", "jsx", "markdown"
-},
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        -- Automatically install missing parsers when entering a buffer
-        auto_install = true,
-        highlight = {
-          enable = true, -- false will disable the whole extension
-        },
-        indent = { enable = true },
-      })
-    end,
-  }
-}
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
 
+			local parsers = {
+				"bash",
+				"css",
+				"fish",
+				"gitignore",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"lua",
+				"markdown",
+				"python",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"xml",
+				"yaml",
+			}
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyDone",
+				once = true,
+				callback = function()
+					require("nvim-treesitter").install(parsers)
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		event = "BufReadPost",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+				move = {
+					set_jumps = true,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "VeryLazy",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			max_lines = 3,
+			min_window_height = 20,
+		},
+		keys = {
+			{
+				"[C",
+				function()
+					require("treesitter-context").go_to_context()
+				end,
+				desc = "go to context",
+			},
+		},
+	},
+}
